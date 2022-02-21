71b0422d49536462f052bf72052c18112767ea47
agholson <87455899+agholson@users.noreply.github.com>
2022-02-20 21:20:36 -0500

    Initial Commit

---
+++ SwiftUIPathsL3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPathsL3
+//
+//  Created by Leone on 2/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPathsL3/SwiftUIPathsL3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPathsL3App.swift
+//  SwiftUIPathsL3
+//
+//  Created by Leone on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPathsL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
